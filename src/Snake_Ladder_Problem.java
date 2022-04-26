389fb152fa1bd2ba90204701c9752ac8f7dcac61
SSK137 <saurabhkodam@gmail.com>
2022-04-26 20:14:49 +0530

    Snake_Ladder:Start With Zero

--- src/Snake_Ladder_Problem.java
+++ src/Snake_Ladder_Problem.java
@@ -1,5 +1,8 @@
+import java.util.*;
 public class Snake_Ladder_Problem {
+    public static final int START_POSITION = 0;
     public static void main(String[] args) {
-        System.out.println("Welcome To Snake and Ladder Problem");
+        System.out.println("Welcome To Snake And Ladder Game");
+        int position= 0;
     }
 }
