e6f364f38d8b7e42a38fdd5bd97a8686f003e9aa
SSK137 <saurabhkodam@gmail.com>
2022-04-26 20:20:31 +0530

    Snake_Ladder: Player Dies

--- src/Snake_Ladder_Problem.java
+++ src/Snake_Ladder_Problem.java
@@ -2,7 +2,12 @@ import java.util.*;
 public class Snake_Ladder_Problem {
     public static final int START_POSITION = 0;
     public static void main(String[] args) {
-        System.out.println("Welcome To Snake And Ladder Game");
+        System.out.println("Welcome To Snake And Ladder");
+        System.out.println("Roll The Dies");
+        Random random = new Random();
         int position= 0;
+        int dies = random.nextInt(6) + 1;
+        System.out.println("Dies Value Is " + dies);
+
     }
 }
